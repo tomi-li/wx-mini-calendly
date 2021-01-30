2c33404cb6f81cc6ec9c9860987127d046a3e49b
Tomi <lishuo.tomi@gmail.com>
2021-01-30 19:44:14 +0800

    init

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,31 +1,38 @@
-import React, { useEffect } from 'react';
-import { View } from 'remax/wechat';
+import React, {useEffect} from 'react';
+import {View, Swiper, Ad} from 'remax/wechat';
 import styles from './index.css';
 
 export default () => {
 
-  useEffect(() => {
-    load()
-  }, []);
+    useEffect(() => {
+        load()
+    }, []);
 
-  return (
-    <View className={styles.app}>
-      <View className={styles.header}>
-        this is wechat version of remax
-      </View>
-    </View>
-  );
+    return (
+        <View className={styles.app}>
+            <View className={styles.header}>
+                <View>
+                    this is wechat version of remax
+                </View>
+                <Ad unitId={"ads"}/>
+                <Swiper></Swiper>
+                this is wechat version of remax
+            </View>
+        </View>
+    );
 };
 
 const load = () => {
-  wx.getUserInfo({
-    success: result => {
-      console.log(result.userInfo);
-    }
-  })
+    wx.getUserInfo({
+        success: result => {
+            console.log(result.userInfo);
+        }
+    })
 
-  console.log(wx.getAccountInfoSync());
-  return wx.request({
-    url: 'http://localhost:8080/v1/user/'
-  })
+    wx.createLivePusherContext()
+
+    console.log(wx.getAccountInfoSync());
+    return wx.request({
+        url: 'http://localhost:8080/v1/user/'
+    })
 };
